169ba85c58549c2f28681bd9015e58f7d694a5c3
Yang <ysqyang@gmail.com>
2018-09-25 18:07:59 +0800

    created class for streaming corpus

--- comment_scoring.py
+++ comment_scoring.py
@@ -4,73 +4,84 @@ import jieba
 from gensim import corpora, models, similarities
 import numpy as np
 
-def tokenize(sentence, stopwords_dict_path):
+def get_stopwords(path):
     '''
-    Tokenize a Chinese sentence to a list of words
+    Creates stopword set in memory from a file 
+    specified by the path arg
     Args:
-    sentence:       sentence to be tokenized
-    stopwords_dict: path for the stopword dictionary
+    path: path for the file containing stopwords
     '''
+    n = 0
     stopwords = set()
     # load stopwords dictionary
-    with open(stopwords_dict_path, 'r') as f:
+    with open(path, 'r') as f:
         while True:
-            stopword = f.readline().strip()
+            stopword = f.readline().rstrip('\n')
             if stopword == '':
                 break
+            n += 1
             stopwords.add(stopword)
 
-    return [word for word in jieba.lcut(sentence) if word not in stopwords]  
+    return stopwords
 
-def sentence_vector(sentence, tokenize_fn):
+def tokenize(text, stopwords_set):
     '''
-    Converts a Chinese sentence to a vector using word embeddings
+    Tokenize Chinese text to a list of words
     Args:
-    sentence:    sentence to be converted
-    tokenize_fn: function to tokenize a sentence
+    text:          text to be tokenized
+    stopwords_set: set of stopwords
     '''
-    words = tokenize_fn(sentence)
+    return [word for word in jieba.lcut(text) if word not in stopwords_set]  
+
+def text_vector(text, tokenize_fn):
+    '''
+    Converts Chinese text to a vector using word embeddings
+    Args:
+    text:        text to be converted
+    tokenize_fn: function to tokenize a text
+    '''
+    words = tokenize_fn(text)
     v = np.zeros(64)      
     
     for word in words:
         v += model[word]
     
-    v /= len(words)
-    
-    return v
+    return v/len(words)
 
-def sentence_similarity(sentence_1, sentence_2, sentence_to_vector_fn, word2vec_model_path):
+def text_similarity(text_1, text_2, text_to_vector_fn, word2vec_model_path):
     '''
-    Computes the similarity between two sentences
+    Computes the similarity between two texts
     Args:
-    sentence_1:             a Chinese sentence tokenized into words
-    sentence_2:             a Chinese sentence tokenized into words
-    sentence_to_vector_fn:  function to convert a sentence to a vector 
-    word2vec_model_path:    path of binary word2vec model file
+    text_1:              Chinese text tokenized into words
+    text_2:              Chinese text tokenized into words
+    text_to_vector_fn:   function to convert a text to a vector 
+    word2vec_model_path: path of binary word2vec model file
     '''
-    model = models.KeyedVectors.load_word2vec_format(word2vec_model_path, binary=True)    
-    v1, v2 = sentence_to_vector_fn(sentence1), sentence_to_vector_fn(sentence2)
+    model = models.KeyedVectors.load_word2vec_format(
+                                word2vec_model_path, binary=True)
+
+    v1, v2 = text_to_vector_fn(text_1), text_to_vector_fn(text_2)
 
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
-def make_similarity_graph(sentences, similarity_fn):
+def make_similarity_graph(texts, similarity_fn):
     '''
-    Creates a undirected graph with sentences as nodes.
-    An edge exists between two sentences if and only they
+    Creates a undirected graph with texts as nodes.
+    An edge exists between two texts if and only they
     have a nonzero similarity score.
     Args:
-    sentences:     list of sentences for which pairwise similarities
+    texts:         list of text id's for which pairwise similarities
                    are to be computed
-    similarity_fn: function to compute the similarity between two sentences
+    similarity_fn: function to compute the similarity between two texts
     '''
 
     G = nx.Graph()  # similarity is symmetric, so the graph is undirected
-    G.add_nodes_from(sentences)
-    num_sentences = len(sentences)
-    for i in range(num_sentences):
-        for j in range(i+1, num_sentences):
-            G.add_edge(sentences[i], sentences[j], 
-                       weight=similarity_fn(sentences[i], sentences[j]))
+    G.add_nodes_from(texts)
+    num_texts = len(texts)
+    for i in range(num_texts):
+        for j in range(i+1, num_texts):
+            G.add_edge(texts[i], texts[j], 
+                       weight=similarity_fn(texts[i], texts[j]))
 
     return G
 
@@ -96,25 +107,36 @@ def comment_importance(graphs, coefficients, alphas):
                zip(graphs, coefficients, alphas)])
 
 corpus = ['我昨天去上海了',
-          '今天天气好热',
-          '他们在犹豫晚上要不要出去吃饭',
-          '你后天要去北京',
+          '今天天气好热，不过下周的天气会很舒服',
+          '他们在犹豫晚上要不要出去吃饭，后来还是在家里吃饭了',
+          '你后天要去北京，北京这几天雾霾很厉害',
           '她儿子考上了清华大学',
           '最近股票跌得很厉害',
           '今年高考她没考好',
           '这两年经济不景气啊',
-          '下周要下雪了']
+          '下周要下雪了'
+          ]
 
-corpus = [tokenize(doc, './stopwords.txt') for doc in corpus]
+stopwords = get_stopwords('./stopwords.txt')
+
+corpus = [tokenize(doc, stopwords) for doc in corpus]
 
 dictionary = corpora.Dictionary(corpus)
 
-corpus = [dictionary.doc2bow(text) for text in corpus]
-print(corpus)
+for id_ in dictionary:
+    print(id_, dictionary[id_])
 
+'''
+corpus = [dictionary.doc2bow(text) for text in corpus]
+
+print(corpus)
+'''
 tfidf = models.TfidfModel(corpus)
 
-print(tfidf[corpus[0]])
+converted = tfidf[corpus]
+
+for doc in converted:
+    print(doc)
 
 
 
--- topic_profiling.py
+++ topic_profiling.py
@@ -1,3 +1,35 @@
 from gensim import corpora, models, similarities
 import jieba
+import comment_scoring
+
+class Corpus_for_streaming(object):
+	def __iter__(self):
+		with open('mycorpus.txt') as f:
+			yield dictionary.doc2bow(line.lower().split())
+
+def term_importance(texts, fetch_text_fn, tokenize_fn, 
+	                weight_fn, model, stopwords_dict):
+	'''
+	Computes word importance in a corpus with weighted docs
+	Args:
+	texts:     list of text id's
+	weight_fn: function to compute weight for each text
+	model:     language model 
+	'''
+
+	corpus = []
+	for id_, text in texts.items():
+		corpus.append(tokenize_fn(text, stopwords_dict))
+
+	dictionary = corpora.Dictionary(corpus)
+	corpus = [dictionary.doc2bow(text) for text in corpus]
+
+	language_model = model(corpus)
+	converted = tfidf[corpus]
+
+	for doc in converted:
+	    print(doc)
+
+
+
 
