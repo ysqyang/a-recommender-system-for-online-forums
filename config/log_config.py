2e938220b404c595d9f986c8794d7e8acad0dc40
Yang <ysqyang@gmail.com>
2018-11-23 17:38:10 +0800

    nothing

--- config/log_config.py
+++ config/log_config.py
@@ -6,5 +6,5 @@ _RUN_LOG_NAME   = 'run'
 _SERVE_LOG_NAME = 'serve'
 _LOGGER_LEVEL   = logging.INFO
 _LEVELS         = {'info': logging.INFO}
-_LOG_FORMAT     = '[%(asctime)s] [%(name)-16s] [%(levelname)-8s] -- %(message)s'
+_LOG_FORMAT     = '[%(asctime)s] [%(name)-10s] [%(levelname)-8s] -- %(message)s'
 _MODE           = 'w'
